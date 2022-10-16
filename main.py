1e3282836913d5ea7397913554b2e441cc030665
al08 <115923486+Alzub08@users.noreply.github.com>
2022-10-16 10:48:41 +0000

    Add obj

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ('hello world')
+class Human:
+  def __init__(self):
+    self.name = 'None'
+    self.age = 0
+    self.gender = 'None'
+  def introduse(self):
+    print('Hello my name is', self.name)
+  def add_info(self):
+    self.name = input('Name: ')
+    self.age = input('Age: ')
+    self.gender = input('Gender: ')
+
+obj = Human()
+obj.introduse()
+obj.add_info()
+obj.introduse()
